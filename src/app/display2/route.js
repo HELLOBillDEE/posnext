b94131ee959a2bbc2da13fe2338a78869354638a
Kanittha Phoothong <n72myw76hp@privaterelay.appleid.com>
2026-08-20 15:48:24 +0700

    Fix: speech rate 0.6

--- src/app/display2/route.js
+++ src/app/display2/route.js
@@ -532,7 +532,7 @@ function speakPayment(st) {
   speechSynthesis.cancel()
   const utt = new SpeechSynthesisUtterance(text)
   utt.lang = 'th-TH'
-  utt.rate = 0.7
+  utt.rate = 0.6
   utt.volume = 1
   // เลือกเสียงผู้หญิงไทยก่อน ถ้าไม่มีค่อยเอาเสียงไทยใดก็ได้
   const voices = speechSynthesis.getVoices()
